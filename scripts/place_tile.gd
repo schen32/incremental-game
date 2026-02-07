43400dd712d95cf40e9ef4d6388ae3a27cffcade
Simon Chen <simonc8192@gmail.com>
2026-02-07 06:42:50 -0800

    Add import configuration for Pop.mp3 sound effect

--- scripts/place_tile.gd
+++ scripts/place_tile.gd
@@ -5,6 +5,8 @@ extends Node2D
 @onready var highlight_controller: Node2D = $"../HighlightTile"
 @onready var ground: TileMapLayer = $"../../Tiles/Ground"
 @onready var player_inventory: Node = $"../../Player/Inventory"
+@onready var player: CharacterBody2D = $"../../Player"
+@onready var place_sound: AudioStreamPlayer2D = $PlaceSound
 
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("place_tile"):
@@ -14,6 +16,10 @@ func try_place() -> void:
 	var cell: Vector2i = highlight_controller.current_hover_cell
 	if cell.x > 100000 or ground.get_cell_source_id(cell) != -1:
 		return
+	
+	var player_cell: Vector2i = ground.local_to_map(ground.to_local(player.global_position))
+	if cell == player_cell:
+		return
 
 	var id: StringName = player_inventory.selected_item
 	if player_inventory.get_amount(id) <= 0:
@@ -22,3 +28,10 @@ func try_place() -> void:
 
 	ground.set_cell(cell, source_id, atlas, 0)
 	player_inventory.remove_item(id, 1)
+	play_place_sound(cell)
+	
+func play_place_sound(cell_pos: Vector2i) -> void:
+	place_sound.pitch_scale = randf_range(0.8, 1.2)
+	place_sound.volume_db = -2 + randf_range(-2.0, 2.0)
+	place_sound.global_position = ground.to_global(ground.map_to_local(cell_pos))
+	place_sound.play()
