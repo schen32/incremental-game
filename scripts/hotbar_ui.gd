dcd8b2d390a545c1d3b81565cd23a2b325323b16
Simon Chen <simonc8192@gmail.com>
2026-02-08 09:01:00 -0800

    Add hotbar and inventory UI; implement toggle functionality and update inventory management

--- scripts/hotbar_ui.gd
+++ scripts/hotbar_ui.gd
@@ -2,13 +2,12 @@ extends Control
 
 @export var slot_scene: PackedScene
 @export var hotbar_size := 10
-@export var total_slots := 30  # includes hotbar
 
 @onready var hotbar_grid: GridContainer = $HotbarPanel/GridContainer
-@onready var inventory_panel: Panel = $InventoryPanel
-@onready var inventory_grid: GridContainer = $InventoryPanel/GridContainer
 @onready var item_name: Label = $ItemName
 @onready var player_inventory: Node = $"../../Player/Inventory"
+
+var expanded := false
 
 func _ready() -> void:
 	player_inventory.changed.connect(_refresh)
@@ -16,7 +15,6 @@ func _ready() -> void:
 
 func _refresh() -> void:
 	_clear_children(hotbar_grid)
-	_clear_children(inventory_grid)
 
 	# --- hotbar row (always shown) ---
 	for i in range(hotbar_size):
@@ -30,17 +28,6 @@ func _refresh() -> void:
 			ui_slot.set_slot(Vector2i(-1, -1), 0)
 
 		ui_slot.set_selected(i == player_inventory.selected_hotbar_index)
-
-	# --- inventory rows ---
-	for i in range(hotbar_size, total_slots):
-		var ui_slot = slot_scene.instantiate()
-		inventory_grid.add_child(ui_slot)
-
-		if player_inventory.slots[i] != null:
-			var stack = player_inventory.slots[i]
-			ui_slot.set_slot(stack.atlas_coords, stack.amount)
-		else:
-			ui_slot.set_slot(Vector2i(-1, -1), 0)
 	
 	var item_data = player_inventory.get_selected_data()
 	item_name.text = &""
---
+++ scripts/toggle_inventory.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+@onready var hotbar: Control = $"../../GameUI/HotbarUI"
+@onready var inventory: Control = $"../../GameUI/InventoryUI"
+
+var inventory_shown := false
+
+func _ready() -> void:
+	hotbar.visible = true
+	inventory.visible = false
+
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("toggle_inventory"):
+		inventory_shown = not inventory_shown
+		hotbar.visible = not inventory_shown
+		inventory.visible = inventory_shown
