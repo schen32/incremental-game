b8dff4a68e2231e30a6239a558b201bcec17287f
Simon Chen <simonc8192@gmail.com>
2026-02-13 17:52:52 -0800

    - rat drops gem on death

--- scripts/components/on_death.gd
+++ scripts/components/on_death.gd
@@ -1,7 +1,10 @@
 extends Node2D
 
+@export var dropped_item: PackedScene
+@export var dropped_item_id: StringName = &"gem"
+@export var dropped_item_amount: int = 1
+
 @onready var health: Node2D = $"../Health"
-@onready var sprite: AnimatedSprite2D = $"../AnimatedSprite2D"
 @onready var body: CharacterBody2D = get_parent()
 @onready var motor: Node2D = $"../Motor"
 @onready var anim_controller: Node2D = $"../AnimationController"
@@ -18,8 +21,15 @@ func _on_died() -> void:
 	dead = true
 
 	motor.freeze()
-
 	anim_controller.play_anim(&"death")
 	SoundManager.play_player(death_sound)
+	
+	for i in range(dropped_item_amount):
+		var item := dropped_item.instantiate()
+		get_tree().current_scene.add_child(item)
+		item.global_position = body.global_position
+		item.set_item(dropped_item_id)
+		item.pop(2)
+	
 	await anim_controller.anim_finished
 	body.queue_free()
--- scripts/entities/dropped_item.gd
+++ scripts/entities/dropped_item.gd
@@ -1,6 +1,9 @@
 extends RigidBody2D
 
 @export var item_id: StringName = &""
+@export var pop_strength := 40.0        # overall force
+@export var pop_spread_x := 0.5         # how wide it can spray horizontally (0–1)
+@export var pop_upward_bias := 1.0      # how strongly it prefers going up (1 = full up)
 
 @onready var sprite: Sprite2D = $Sprite2D
 @onready var collision_shape: CollisionShape2D = $CollisionShape2D
@@ -9,13 +12,17 @@ func _ready() -> void:
 	if item_id != &"":
 		set_item(item_id)
 
-func set_item(_item_id: StringName) -> void:
+func set_item(_item_id: StringName) -> void:	
 	item_id = _item_id
 	var item_data: ItemData = ItemDatabase.get_item(_item_id)
-	
+
 	sprite.texture = item_data.texture
 	sprite.region_enabled = true
 	sprite.region_rect = item_data.get_region()
-		
-func pop() -> void:
-	apply_impulse(Vector2(randf_range(-16, 16), randf_range(-42, -26)))
+
+func pop(multiplier := 1.0) -> void:
+	var x_dir := randf_range(-pop_spread_x, pop_spread_x)
+	var y_dir := -pop_upward_bias
+	var dir := Vector2(x_dir, y_dir).normalized()
+
+	apply_impulse(dir * pop_strength * multiplier)
